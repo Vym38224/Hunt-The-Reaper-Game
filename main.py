7d900dcea5511d72868f06390f5ae046e2d7d3dc
Vym38224 <jardaavymetal@gmail.com>
2023-03-15 11:47:11 +0100

    progres2

--- main.py
+++ main.py
@@ -24,10 +24,6 @@ grey = (130,130,130)
 # Nastavení 
 distance = 9
 clock = pygame.time.Clock()
-fps_bg = pygame.Surface((25,25))
-fps_bg.fill((255,255,255))
-fps_font = pygame.font.SysFont("Arial", 20)
-
 mirror = 0
 fire_attack = 0
 click_x = 0
@@ -37,24 +33,55 @@ fball_1x = -1* fball_x
 fball_speed = 9
 
  
-# Background + Obrázky
-background_img = loadify("img/background.png")
+#### BACKGROUND, OBRÁZKY, TEXTY ####
+# BG
+background_img = loadify(background.img)
 background_img_rect = background_img.get_rect()
 
+# IMG
+# Firemag
 fzard_image = loadify(player1.img)
 fzard_rect = fzard_image.get_rect()
 fzard_rect.center = (width//2, height//2)
-fzard2_image = loadify("img/firemag2.png")
+
+fzard2_image = loadify(player1.img2)
 fzard2_rect = fzard_image.get_rect()
 fzard2_rect.center = (width//2, height//2)
 
-fball_image = loadify("img/fireball.png")
+fball_image = loadify(player1.img_attack)
 fball_rect = fzard_image.get_rect()
-fball2_image = loadify("img/fireball2.png")
+
+fball2_image = loadify(player1.img_attack2)
 fball2_rect = fzard_image.get_rect()
 
+# Watermag
+wzard_image = loadify(player2.img)
+wzard_rect = wzard_image.get_rect()
+wzard_rect.center = (width//2, height//2)
 
-# Hlavní cyklus
+wzard2_image = loadify(player2.img2)
+wzard2_rect = wzard_image.get_rect()
+wzard2_rect.center = (width//2, height//2)
+
+wzard_attack_image = loadify(player2.img_attack)
+wzard_attack_rect = wzard_attack_image.get_rect()
+
+wzard_attack2_image = loadify(player2.img_attack2)
+wzard_attack2_rect = wzard_attack2_image.get_rect()
+
+# TXT
+name_text = pygame.font.SysFont("Moncerat", 20)
+name_text = name_text.render(player1.name, True, white)
+name_text_rect = name_text.get_rect()
+name_text_rect.center = (600,440)
+
+lvl_text = pygame.font.SysFont("Moncerat", 16)
+lvl_text = lvl_text.render("lvl " + str(player1.level), True, yellow)
+lvl_text_rect = lvl_text.get_rect()
+lvl_text_rect.center = (600,450)
+
+
+#### HLAVNÍ CYKLUS ####
 lets_continue = True
 while lets_continue:
     for event in pygame.event.get():
@@ -63,23 +90,30 @@ while lets_continue:
 
         screen.blit(background_img, background_img_rect)
         
-
         keys = pygame.key.get_pressed()
         if keys [pygame.K_w]:
             fzard_rect.y = fzard_rect.y - distance
             fzard2_rect.y = fzard2_rect.y - distance
+            name_text_rect.y = name_text_rect.y - distance  
+            lvl_text_rect.y = lvl_text_rect.y - distance
         elif keys [pygame.K_s]:
             fzard_rect.y = fzard_rect.y + distance
             fzard2_rect.y = fzard2_rect.y + distance
+            name_text_rect.y = name_text_rect.y + distance
+            lvl_text_rect.y = lvl_text_rect.y + distance
         elif keys [pygame.K_a]:
             fzard_rect.x = fzard_rect.x - distance
             fzard2_rect.x = fzard2_rect.x - distance
+            name_text_rect.x = name_text_rect.x - distance
+            lvl_text_rect.x = lvl_text_rect.x - distance
             fzard2_rect.center = (fzard_rect.x + 60,fzard_rect.y + 60)
             mirror = 1
             fire_attack = 0
         elif keys [pygame.K_d]:
             fzard_rect.x = fzard_rect.x + distance
             fzard2_rect.x = fzard2_rect.x + distance
+            name_text_rect.x = name_text_rect.x + distance
+            lvl_text_rect.x = lvl_text_rect.x + distance
             mirror = 0
             fire_attack = 0
         elif keys [pygame.K_SPACE]:
@@ -97,11 +131,15 @@ while lets_continue:
         
     if mirror == 0:
         screen.blit(fzard_image,fzard_rect)
+        screen.blit(name_text,name_text_rect)
+        screen.blit(lvl_text,lvl_text_rect)
     if mirror == 1:
         screen.blit(fzard2_image,fzard2_rect)
+        screen.blit(name_text,name_text_rect)
+        screen.blit(lvl_text,lvl_text_rect)
 
     pygame.display.update()
-    clock.tick_busy_loop(144)
+    clock.tick_busy_loop(60)
 
 # Ukončení hry
 pygame.quit()
--- players.py
+++ players.py
@@ -1,20 +1,26 @@
-import pygame
+class Background:
+    def __init__(self, img):
+        self.img = img
+
 
 class Player:
-
-    def __init__(self, name, level, img, in_map = False):
-        self.img = img
+    def __init__(self, name, level, img, img2, img_attack, img_attack2, in_map = False):
         self.name = name
+        self.level = level
+        self.img = img
+        self.img2 = img2
+        self.img_attack = img_attack
+        self.img_attack2 = img_attack2
         if in_map == True:
-            self.level = level
-    
+            pass
+              
     def attack(self):
         return "Útok"
 
-class WizardPlayer(Player):
 
-    def __init__(self, element, name, level, img):
-        super().__init__(name, level, img, in_map = False)
+class WizardPlayer(Player):
+    def __init__(self, name, element, level, img, img2, img_attack, img_attack2):
+        super().__init__(name, level, img, img2, img_attack, img_attack2, in_map = False)
         self.element = element
         
     def attack(self):
@@ -22,22 +28,25 @@ class WizardPlayer(Player):
     
 
 class ArcherPlayer(Player):
-
-    def __init__(self, animal, name, level):
+    def __init__(self, name, animal, level):
         super().__init__(name, level, in_map = False)
         self.animal = animal
         
     def attack(self):
         return "Útok ->"
     
+
+#Background
+background = Background(("img/background.png"))
+    
 #lidi
-#player0 = Player("Sam", 1)
+player0 = Player("Sam", 1,("img/human.png"),("img/human_attack.png"))
 
 #kouzelníci
-player1 = WizardPlayer("Dave","fire", 1,("img/firemag.png"))
+player1 = WizardPlayer("Dave","fire", 1,("img/firemag.png"),("img/firemag2.png"),("img/fireball.png"),("img/fireball2.png"))
+player2 = WizardPlayer("Anna", "water", 1,("img/watermag.png"),("img/watermag2.png"),("img/watermag_attack.png"),("img/watermag_attack2.png"))
 
-"""player2 = WizardPlayer("Anna", "water", 1)
-player3 = WizardPlayer("Ronald", "earth", 1)
+"""player3 = WizardPlayer("Ronald", "earth", 1)
 player4 = WizardPlayer("Jinora","wind", 1)
 player5 = WizardPlayer("Amon" ,"black magic", 1)
 
