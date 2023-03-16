6dd7e683287ecbbef76f41992c5379e80bf1f9fe
Vym38224 <jardaavymetal@gmail.com>
2023-03-16 23:54:30 +0100

    progres3

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pygame
+import random
 from players import *
 
 # načtení IMG 
 def loadify(imgname):
-    return pygame.image.load(imgname).convert_alpha()
+    return pygame.image.load(imgname).convert_alpha() 
 
 # Inicializace
 pygame.init()
@@ -21,23 +22,65 @@ dark_green = (0, 100, 0)
 blue = (0, 0, 255)
 yellow = (255, 255, 0)
 grey = (130,130,130)
+green = (124, 252, 0)
     
 # Nastavení
 fps = 60
 clock = pygame.time.Clock()
+mag = 1
+fmag = 1
+wmag = 0
+scelet = 1
+
 mirror = 0
 fire_attack = 0
 wire_attack = 0
+
 attack_x = 1
 attack_1x = -1* attack_x
+
+scelet_x = random.choice ([-1,1])
+scelet_y = random.choice([-1,1])
+
+scelet_speed = 4
 attack_speed = 8
 distance = 9
-mag = 1
-fmag = 0
-wmag = 1
+
+fmag_hp = (player1.hp)
+if fmag == 1:
+    str_fmag_hp = str(fmag_hp)
+    try:
+        f1 = open("fmag_hp.txt", "r")
+    except FileNotFoundError:
+        print("Soubor nebyl nalezen")
+    text = f1.read(fmag_hp)
+    f1.close()
+
+wmag_hp = (player2.hp)
+if wmag == 1:
+    str_wmag_hp = str(wmag_hp)
+    try:
+        f2 = open("wmag_hp.txt", "r")
+    except FileNotFoundError:
+        print("Soubor nebyl nalezen")
+    text = f2.read(wmag_hp)
+    f2.close()
+
+scelet_hp = (enemy0.hp)
+if scelet == 1:
+    str_scelet_hp = str(scelet_hp)
+    try:
+        f3 = open("scelet_hp.txt", "r")
+    except FileNotFoundError:
+        print("Soubor nebyl nalezen")
+    text = f3.read(scelet_hp)
+    f3.close()
+
 
  
 #### BACKGROUND, OBRÁZKY, TEXTY ####
+
+
 # BG
 background_img = loadify(background.img)
 background_img_rect = background_img.get_rect()
@@ -63,13 +106,6 @@ name1_text = name1_text.render(player1.name, True, white)
 name1_text_rect = name1_text.get_rect()
 name1_text_rect.center = (600,440)
 
-lvl1_text = pygame.font.SysFont("Moncerat", 16)
-lvl1_text = lvl1_text.render("lvl " + str(player1.level), True, yellow)
-lvl1_text_rect = lvl1_text.get_rect()
-lvl1_text_rect.center = (600,450)
-
-
-
 
 # Watermag
 wzard_image = loadify(player2.img)
@@ -91,14 +127,22 @@ name2_text = name2_text.render(player2.name, True, white)
 name2_text_rect = name2_text.get_rect()
 name2_text_rect.center = (592,435)
 
-lvl2_text = pygame.font.SysFont("Moncerat", 16)
-lvl2_text = lvl2_text.render("lvl " + str(player2.level), True, yellow)
-lvl2_text_rect = lvl2_text.get_rect()
-lvl2_text_rect.center = (588,445)
+# Sceleton
+
+scelet_image = loadify(enemy0.img)
+scelet_rect = scelet_image.get_rect()
+scelet_rect.center = (width//2 + 400, height//2 )
+
+e_name_text = pygame.font.SysFont("Moncerat", 20)
+e_name_text = e_name_text.render(enemy0.name, True, red)
+e_name_text_rect = e_name_text.get_rect()
+e_name_text_rect.center = (992,435)
 
 
 
 #### HLAVNÍ CYKLUS ####
+
+
 lets_continue = True
 while lets_continue:
     for event in pygame.event.get():
@@ -107,108 +151,208 @@ while lets_continue:
 
     screen.blit(background_img, background_img_rect)
 
+
+### POSTAVY ###
+
+
     # Firemag
     if mag == 1 and fmag == 1:
+        hp1_text = pygame.font.SysFont("Moncerat", 18)
+        if fmag_hp >= 75:
+            hp1_text = hp1_text.render("HP: " + f"{fmag_hp}", True, green)
+        if fmag_hp >= 37.5 and fmag_hp < 75:
+            hp1_text = hp1_text.render("HP: " + f"{fmag_hp}", True, yellow)
+        if fmag_hp >= 0 and fmag_hp < 37.5:
+            hp1_text = hp1_text.render("HP: " + f"{fmag_hp}", True, red)
+        if fmag_hp < 0:
+            hp1_text = hp1_text.render("HP (seš mrtvej sračko): " + f"{fmag_hp}", True, red)
+        hp1_text_rect = hp1_text.get_rect()
+        hp1_text_rect.center = (fzard_rect.x + 60, fzard_rect.y + 135)
         keys = pygame.key.get_pressed()
         if keys [pygame.K_w] and fzard_rect.top > 0 and fzard2_rect.top > 0:
             fzard_rect.y = fzard_rect.y - distance
             fzard2_rect.y = fzard2_rect.y - distance
             name1_text_rect.y = name1_text_rect.y - distance  
-            lvl1_text_rect.y = lvl1_text_rect.y - distance
+            hp1_text_rect.y = hp1_text_rect.y - distance
         elif keys [pygame.K_s] and fzard_rect.bottom < height - 120 and fzard2_rect.bottom < height - 120:
             fzard_rect.y = fzard_rect.y + distance
             fzard2_rect.y = fzard2_rect.y + distance
             name1_text_rect.y = name1_text_rect.y + distance
-            lvl1_text_rect.y = lvl1_text_rect.y + distance
+            hp1_text_rect.y = hp1_text_rect.y + distance
         elif keys [pygame.K_a] and fzard_rect.left > 0 and fzard2_rect.left > 0:
             fzard_rect.x = fzard_rect.x - distance
             fzard2_rect.x = fzard2_rect.x - distance
             name1_text_rect.x = name1_text_rect.x - distance
-            lvl1_text_rect.x = lvl1_text_rect.x - distance    
+            hp1_text_rect.x = hp1_text_rect.x - distance    
             mirror = 1
         elif keys [pygame.K_d] and fzard_rect.right > 0 and fzard2_rect.right < width:
             fzard_rect.x = fzard_rect.x + distance
             fzard2_rect.x = fzard2_rect.x + distance
             name1_text_rect.x = name1_text_rect.x + distance
-            lvl1_text_rect.x = lvl1_text_rect.x + distance
+            hp1_text_rect.x = hp1_text_rect.x + distance
             mirror = 0 
 
-        elif keys [pygame.K_SPACE]:
-            if mirror == 1:
-                fball2_rect.center = (fzard2_rect.x + 20,fzard2_rect.y + 35)
-                fire_attack = 1
-            if mirror == 0:
-                fball_rect.center = (fzard_rect.x + 100,fzard_rect.y + 35)
-                fire_attack = 2
-        
-        elif fire_attack == 1:
+        elif keys [pygame.K_SPACE] and mirror == 1:
+            fball2_rect.center = (fzard2_rect.x + 20,fzard2_rect.y + 35)
+            fire_attack = 1
+        elif keys [pygame.K_SPACE] and mirror == 0:
+            fball_rect.center = (fzard_rect.x + 100,fzard_rect.y + 35)
+            fire_attack = 2
+            
+        if fire_attack == 1:
             screen.blit(fball2_image,fball2_rect)
             fball2_rect.x += attack_1x * attack_speed
-        elif fire_attack == 2:
+        if fire_attack == 2:
             screen.blit(fball_image,fball_rect)
-            fball_rect.x += attack_x * attack_speed         
-        
+            fball_rect.x += attack_x * attack_speed 
+    
         if mirror == 0:
             screen.blit(fzard_image,fzard_rect)
             screen.blit(name1_text,name1_text_rect)
-            screen.blit(lvl1_text,lvl1_text_rect)
+            screen.blit(hp1_text,hp1_text_rect)
         if mirror == 1:
             screen.blit(fzard2_image,fzard2_rect)
             screen.blit(name1_text,name1_text_rect)
-            screen.blit(lvl1_text,lvl1_text_rect)
-
+            screen.blit(hp1_text,hp1_text_rect)
 
     # Water mag
     if mag == 1 and wmag == 1:
+        hp2_text = pygame.font.SysFont("Moncerat", 18)
+        if wmag_hp >= 150:
+            hp2_text = hp2_text.render("HP: " + f"{wmag_hp}", True, green)
+        if wmag_hp >= 50 and wmag_hp < 150:
+            hp2_text = hp2_text.render("HP: " + f"{wmag_hp}", True, yellow)
+        if wmag_hp >= 0 and wmag_hp < 50:
+            hp2_text = hp2_text.render("HP: " + f"{wmag_hp}", True, red)
+        if wmag_hp < 0:
+            hp2_text = hp2_text.render("HP (seš mrtvej sračko): " + f"{wmag_hp}", True, red)
+        hp2_text_rect = hp2_text.get_rect()
+        hp2_text_rect.center = (wzard_rect.x + 60, wzard_rect.y + 130)
         keys = pygame.key.get_pressed()
         if keys [pygame.K_w] and wzard_rect.top > 0 and wzard2_rect.top > 0:
             wzard_rect.y = wzard_rect.y - distance
             wzard2_rect.y = wzard2_rect.y - distance
             name2_text_rect.y = name2_text_rect.y - distance  
-            lvl2_text_rect.y = lvl2_text_rect.y - distance
+            hp2_text_rect.y = hp2_text_rect.y - distance
         elif keys [pygame.K_s] and wzard_rect.bottom < height - 120 and wzard2_rect.bottom < height - 120:
             wzard_rect.y = wzard_rect.y + distance
             wzard2_rect.y = wzard2_rect.y + distance
             name2_text_rect.y = name2_text_rect.y + distance
-            lvl2_text_rect.y = lvl2_text_rect.y + distance
+            hp2_text_rect.y = hp2_text_rect.y + distance
         elif keys [pygame.K_a] and wzard_rect.left > 0 and wzard2_rect.left > 0:
             wzard_rect.x = wzard_rect.x - distance
             wzard2_rect.x = wzard2_rect.x - distance
             name2_text_rect.x = name2_text_rect.x - distance
-            lvl2_text_rect.x = lvl2_text_rect.x - distance
+            hp2_text_rect.x = hp2_text_rect.x - distance
             mirror = 1
         elif keys [pygame.K_d] and wzard_rect.right > 0 and wzard2_rect.right < width:
             wzard_rect.x = wzard_rect.x + distance
             wzard2_rect.x = wzard2_rect.x + distance
             name2_text_rect.x = name2_text_rect.x + distance
-            lvl2_text_rect.x = lvl2_text_rect.x + distance
+            hp2_text_rect.x = hp2_text_rect.x + distance
             mirror = 0       
               
-        elif keys [pygame.K_SPACE]:
-            if mirror == 1:
-                wzard_attack2_rect.center = (wzard2_rect.x + 20,wzard2_rect.y + 35)
-                wire_attack = 1
-            if mirror == 0:
-                wzard_attack_rect.center = (wzard_rect.x + 100,wzard_rect.y + 35)
-                wire_attack = 2
+        elif keys [pygame.K_SPACE] and mirror == 1:
+            wzard_attack2_rect.center = (wzard2_rect.x + 10,wzard2_rect.y + 70)
+            wire_attack = 1
+        elif keys [pygame.K_SPACE] and mirror == 0:
+            wzard_attack_rect.center = (wzard_rect.x + 125,wzard_rect.y + 70)
+            wire_attack = 2
         
-        elif wire_attack == 1:
+        if wire_attack == 1:
             screen.blit(wzard_attack2_image,wzard_attack2_rect)
             wzard_attack2_rect.x += attack_1x * attack_speed
-        elif wire_attack == 2:
+        if wire_attack == 2:
             screen.blit(wzard_attack_image,wzard_attack_rect)
             wzard_attack_rect.x += attack_x * attack_speed         
         
         if mirror == 0:
             screen.blit(wzard_image,wzard_rect)
             screen.blit(name2_text,name2_text_rect)
-            screen.blit(lvl2_text,lvl2_text_rect)
+            screen.blit(hp2_text,hp2_text_rect)
         if mirror == 1:
             screen.blit(wzard2_image,wzard2_rect)
             screen.blit(name2_text,name2_text_rect)
-            screen.blit(lvl2_text,lvl2_text_rect)
+            screen.blit(hp2_text,hp2_text_rect)
+        
+    # Sceleton
+    if scelet == 1:
+        hp3_text = pygame.font.SysFont("Moncerat", 18)
+        if scelet_hp > 0:      
+            hp3_text = hp3_text.render("HP: " + f"{scelet_hp}", True, white)
+        if scelet_hp <= 0:
+            hp3_text = hp3_text.render("HP (seš mrtvej sračko): " + f"{scelet_hp}", True, red)
+        hp3_text_rect = hp3_text.get_rect()
+        hp3_text_rect.center = (scelet_rect.x + 30, scelet_rect.y + 125)
+        e_name_text_rect.center = (scelet_rect.x + 30, scelet_rect.y + 110)
+        screen.blit(scelet_image,scelet_rect)
+        screen.blit(hp3_text,hp3_text_rect)
+        screen.blit(e_name_text,e_name_text_rect)
+        scelet_rect.x += scelet_x * scelet_speed
+        scelet_rect.y += scelet_y * scelet_speed
+        if scelet_rect.left < 0 or scelet_rect.left > width - 120:
+            scelet_x = -1 * scelet_x
+        elif scelet_rect.top < 25 or scelet_rect.bottom > height - 120:
+            scelet_y = -1 * scelet_y
+
+        if scelet_rect.left < 0: 
+            scelet_image = loadify(enemy0.img2)
+                
             
-    
+        if scelet_rect.left >= 1000: 
+            scelet_image = loadify(enemy0.img)
+            
+        
+
+
+### KONTROLA KOLIZE ###
+
+
+    if fball_rect.colliderect(scelet_rect) or fball2_rect.colliderect(scelet_rect):
+        if scelet == 1:
+            scelet_hp -= 1
+            str_scelet_hp = str(scelet_hp)
+            try:
+                f3 = open("scelet_hp.txt","w")
+            except FileNotFoundError:
+                    print("Soubor nebyl nalezen")
+            text = f3.write(str_scelet_hp)
+            f3.close()
+
+    if wzard_attack_rect.colliderect(scelet_rect) or wzard_attack2_rect.colliderect(scelet_rect):
+        if scelet == 1:
+            scelet_hp -= 1
+            str_scelet_hp = str(scelet_hp)
+            try:
+                f3 = open("scelet_hp.txt","w")
+            except FileNotFoundError:
+                    print("Soubor nebyl nalezen")
+            text = f3.write(str_scelet_hp)
+            f3.close()
+
+    if scelet_rect.colliderect(wzard_rect) or scelet_rect.colliderect(wzard2_rect):
+        if wmag == 1:
+            wmag_hp -= 1
+            str_wmag_hp = str(wmag_hp)
+            try:
+                f2 = open("wmag_hp.txt","w")
+            except FileNotFoundError:
+                    print("Soubor nebyl nalezen")
+            text = f2.write(str_wmag_hp)
+            f2.close()
+        if fmag == 1:
+            fmag_hp -= 1
+            str_fmag_hp = str(fmag_hp)
+            try:
+                f1 = open("fmag_hp.txt","w")
+            except FileNotFoundError:
+                    print("Soubor nebyl nalezen")
+            text = f1.write(str_fmag_hp)
+            f1.close()
+
+    if wmag_hp == 0:
+        print("nemáš hapka debile")
+
     pygame.display.update()
     clock.tick_busy_loop(fps)
 
--- players.py
+++ players.py
@@ -4,9 +4,9 @@ class Background:
 
 
 class Player:
-    def __init__(self, name, level, img, img2, img_attack, img_attack2, in_map = False):
+    def __init__(self, name, hp, img, img2, img_attack, img_attack2, in_map = False):
         self.name = name
-        self.level = level
+        self.hp = hp
         self.img = img
         self.img2 = img2
         self.img_attack = img_attack
@@ -19,8 +19,8 @@ class Player:
 
 
 class WizardPlayer(Player):
-    def __init__(self, name, element, level, img, img2, img_attack, img_attack2):
-        super().__init__(name, level, img, img2, img_attack, img_attack2, in_map = False)
+    def __init__(self, name, element, hp, img, img2, img_attack, img_attack2):
+        super().__init__(name, hp, img, img2, img_attack, img_attack2, in_map = False)
         self.element = element
         
     def attack(self):
@@ -28,13 +28,21 @@ class WizardPlayer(Player):
     
 
 class ArcherPlayer(Player):
-    def __init__(self, name, animal, level):
-        super().__init__(name, level, in_map = False)
+    def __init__(self, name, animal, hp):
+        super().__init__(name, hp, in_map = False)
         self.animal = animal
         
     def attack(self):
         return "Útok ->"
     
+class EnemyPlayer:
+    def __init__(self, name, hp, img, img2):
+        self.name = name
+        self.hp = hp
+        self.img = img
+        self.img2 = img2
+
+
 
 #Background
 background = Background(("img/background.png"))
@@ -43,8 +51,8 @@ background = Background(("img/background.png"))
 player0 = Player("Sam", 1,("img/human.png"),("img/human_attack.png"),("img/human2.png"),("img/human_attack2.png"))
 
 #kouzelníci
-player1 = WizardPlayer("Dave","fire", 1,("img/firemag.png"),("img/firemag2.png"),("img/fireball.png"),("img/fireball2.png"))
-player2 = WizardPlayer("Anna", "water", 1,("img/watermag.png"),("img/watermag2.png"),("img/watermag_attack.png"),("img/watermag_attack2.png"))
+player1 = WizardPlayer("Dave","fire", 100,("img/firemag.png"),("img/firemag2.png"),("img/fireball.png"),("img/fireball2.png"))
+player2 = WizardPlayer("Anna", "water", 200,("img/watermag.png"),("img/watermag2.png"),("img/watermag_attack.png"),("img/watermag_attack2.png"))
 
 """player3 = WizardPlayer("Ronald", "earth", 1)
 player4 = WizardPlayer("Jinora","wind", 1)
@@ -54,6 +62,10 @@ player5 = WizardPlayer("Amon" ,"black magic", 1)
 player6 = ArcherPlayer("Robin", "bird", 1)
 player7 = ArcherPlayer("Parek", "wolf", 1)
 player8 = ArcherPlayer("Amy", "bear", 1)"""
+
+#enemáci
+ 
+enemy0 = EnemyPlayer("Kostlivec", 20,("img/sceleton.png"),("img/sceleton2.png") )
 
 
 
